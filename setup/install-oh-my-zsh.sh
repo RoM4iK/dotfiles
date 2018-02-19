1e4a3e67c78d046d622ccbd513771c733478e6c8
Roman Gritsay <roman@gritsay.io>
2018-02-19 22:05:49 +0200

    Update install-oh-my-zsh.sh

--- setup/install-oh-my-zsh.sh
+++ setup/install-oh-my-zsh.sh
@@ -1,5 +1,5 @@
-sudo apt-get install zsh
-sudo apt install python3-dev python3-pip
+sudo apt install zsh -y
+sudo apt install python3-dev python3-pip -y
 sudo pip3 install thefuck
 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
